9a907b822fe7b3ab2e2bdb66d6fbcfc60b6cfdda
Achintya1800 <achintya.chavan22@spit.ac.in>
2025-02-27 19:00:01 +0530

    add upload file route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,9 +4,10 @@ import sendOtp  from '../controllers/sendOtp.js';
 import verifyOtp  from '../controllers/verifyOtp.js';
 import setMPIN from '../controllers/setMPIN.js';
 import loginUser from '../controllers/login.js';
-
+import upload from '../controllers/upload.js';
 router.post('/otp',sendOtp);
 router.post('/verify',verifyOtp);
 router.post('/mpin',setMPIN);    
 router.post('/login',loginUser);
+router.post('/uploads',upload);
 export default router;
